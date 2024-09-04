aca470337939ac3674bacc9e177005cbd5cf7424
Arpitha S <145808037+Arpitha-004@users.noreply.github.com>
2024-09-05 01:51:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Quick Sort/quick-sort.java
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+import java.util.*;
+class Sorting
+{
+	static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+    
+    // Driver program
+    public static void main(String args[])
+    {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while(T>0)
+		{
+			int n = sc.nextInt();
+			int arr[] = new int[n];
+			for(int i=0;i<n;i++)
+				arr[i] = sc.nextInt();
+
+			
+			new Solution().quickSort(arr,0,n-1);
+			printArray(arr);
+		    T--;
+		}
+} }
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to sort an array using quick sort algorithm.
+    static void quickSort(int arr[], int low, int high)
+    {
+        // code here
+        if(low < high){
+            int p = partition(arr,low,high);
+            quickSort(arr,low,p-1);
+            quickSort(arr,p+1,high);
+        }
+        
+    }
+    static int partition(int arr[], int low, int high)
+    {
+        // your code here
+        int p = arr[low];
+        int i = low;
+        int j = high;
+        
+        while(i<j){
+            while(i<=high && arr[i]<=p){
+                i++;
+            }
+            while(j>=low && arr[j]>p){
+                j--;
+            }
+            if (i < j) {
+                swap(arr, i, j);
+            }
+        }
+        
+        swap(arr,low,j);
+        
+        return j;
+    }
+    static void swap(int[] arr,int a,int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+}
