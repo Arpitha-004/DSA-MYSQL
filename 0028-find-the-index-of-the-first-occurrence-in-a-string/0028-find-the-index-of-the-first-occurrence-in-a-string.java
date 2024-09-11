dcf4512a29ed94b879ce393b5f434ad4434f137c
Arpitha S <145808037+Arpitha-004@users.noreply.github.com>
2024-09-11 19:53:32 +0530

    Time: 0 ms (100%), Space: 41.4 MB (65.22%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -3,8 +3,13 @@ class Solution {
         if(needle.length() == 0)
             return -1;
         
-        if(haystack.contains(needle)){
-            return haystack.indexOf(needle);
+        int len = haystack.length() - needle.length() + 1;
+        for(int i=0;i<len;i++){
+            if(haystack.charAt(i) == needle.charAt(0)){
+                if(haystack.substring(i,needle.length()+i).equals(needle)){
+                    return i;
+                }
+            }
         }
         return -1;
     }
