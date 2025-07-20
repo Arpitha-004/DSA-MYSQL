9d394ce837e21698c6ec373bbddc899199aa3e58
Arpitha S <145808037+Arpitha-004@users.noreply.github.com>
2025-07-20 21:16:57 +0530

    Time: 5 ms (56.48%), Space: 43 MB (81.83%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,20 +1,18 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length())
+        if(s.length()!=t.length())
             return false;
-        
-        int[] alp_cnt = new int[26];
 
+        int cnt[] = new int[26];
         for(int i=0;i<s.length();i++){
-            alp_cnt[s.charAt(i) - 'a']++;
-            alp_cnt[t.charAt(i) - 'a']--;
+            cnt[s.charAt(i)-'a']++;
+            cnt[t.charAt(i)-'a']--;
         }
-
-        for(int cnt : alp_cnt){
-            if(cnt != 0)
+        for(int i : cnt){
+            if(i!=0){
                 return false;
+            }
         }
-
         return true;
     }
 }
