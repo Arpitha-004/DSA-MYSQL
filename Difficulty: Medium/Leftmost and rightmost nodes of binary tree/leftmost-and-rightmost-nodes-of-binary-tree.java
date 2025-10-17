dffd7ac631cd6df28614d82af7d3a88a3b6c0237
Arpitha S <145808037+Arpitha-004@users.noreply.github.com>
2025-10-18 02:12:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Leftmost and rightmost nodes of binary tree/leftmost-and-rightmost-nodes-of-binary-tree.java
+++ Difficulty: Medium/Leftmost and rightmost nodes of binary tree/leftmost-and-rightmost-nodes-of-binary-tree.java
@@ -14,26 +14,22 @@ class Tree {
     public static void printCorner(Node node) {
 
         // add your code here
-        List<List<Integer>> lst = new ArrayList<>();
-        sideview(lst,node,0);
-        for(int i=0;i<lst.size();i++){
-            int n = lst.get(i).size();
-            System.out.print(lst.get(i).get(0)+" ");
-            if(n>1)
-                System.out.print(lst.get(i).get(n-1)+" ");
-            
+        Queue<Node> q = new LinkedList<>();
+        q.add(node);
+        
+        while(!q.isEmpty()){
+            int n = q.size();
+            for(int i=0;i<n;i++){
+                Node temp = q.peek();
+                if(i==0 || i==n-1)
+                    System.out.print(temp.data +" ");
+                    
+                q.poll();
+                if(temp.left!=null)
+                    q.add(temp.left);
+                if(temp.right!=null)
+                    q.add(temp.right);
+            }
         }
-    }
-    public static void sideview(List<List<Integer>> lst, Node root, int level){
-        if(root==null)
-            return;
-    
-        if(lst.size()==level)
-            lst.add(new ArrayList<>());
-            
-        lst.get(level).add(root.data);
-    
-        sideview(lst,root.left,level+1);
-        sideview(lst,root.right,level+1);
     }
 }
